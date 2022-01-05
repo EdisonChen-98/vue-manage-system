aa5ff9563b6e618898a0e224aec69da975edfbd6
Edison_Chen <2508241971@qq.com>
2022-01-06 00:58:40 +0800

    feat:init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">你好</div>
+</template>
+
+<script>
+import http from "./util/http";
+export default {
+  name: "App",
+  components: {},
+  mounted() {
+    http.get("test", "");
+  },
+};
+</script>
+
+<style>
+</style>
